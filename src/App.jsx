00654721e8170f892124cb282ccc3689eff20019
RohhaanMane <rohhaanmane@gmail.com>
2024-12-23 00:13:55 +0530

    new one

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import "./App.css"; // Make sure to style your button and main page appropriately
+import RiddleCard from "./components/RiddleCard";
+
+const App = () => {
+  const [clickCount, setClickCount] = useState(0);
+
+  const messages = [
+    "Hi Karishma!🙋‍♂️",
+    "Curious to Know me?😁",
+    "Chal Ek koda solve kr",
+    "Almost there!",
+    "Hi",
+    "",
+    
+  ];
+
+  const images = [
+    "src/assets/1_fox.jpg", // Replace with actual image paths
+    "src/assets/2_fox.avif",
+    "src/assets/5_fox.jpg",
+    "src/assets/3_fox.avif",
+    "src/assets/4_fox.jpg",
+    "src/assets/6_fox.jpg",
+  ];
+
+  const handleClick = () => {
+    setClickCount(clickCount + 1);
+  };
+
+  return (
+    <div className="app-container">
+      {clickCount < messages.length - 1 ? (
+        <div className="button-container">
+          {images[clickCount] && (
+            <img
+              src={images[clickCount]}
+              alt="Cartoon"
+              className="cartoon-image"
+            />
+          )}
+          <h2>{messages[clickCount]}</h2>
+
+          <button className="emoji-button" onClick={handleClick}>
+            😊 Click Me!
+          </button>
+        </div>
+      ) : (
+        <div className="main-page">
+          <h1>{messages[messages.length - 1]}</h1>
+          {/* <p>Here's your main page content!</p> */}
+          <RiddleCard />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/RiddleCard.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import './riddleCard.css'
+
+const RiddleCard = () => {
+  return (
+    <>
+
+    <img className='cartoonImg' src='src/assets/7_cat.avif'/>
+    <div className='riddle-card'>
+      <div className='headerName'>
+        <span>◀️▶️</span>
+        <h4>He Ghe sodav</h4>
+      </div>
+      <p>
+      <span className='newSpan'>NO. :- </span>
+        Rec ke Dil me hoon, </p>
+      <p>Dil ke bahar bhi... </p>
+      <p>Hai sb apne, </p>
+      <p>Lagu thoda odd bhi... </p>
+      <p>Developer hoon, </p>
+      <p>Develop kru dil me pyaar bhi...</p>
+      <p>Kehta hoon khudko, </p>
+      <p>Calm demeanor... </p>
+      <p>Let's see, If </p>
+      <p>You can find me sooner... </p>
+    </div>
+    </>
+  )
+}
+
+export default RiddleCard
